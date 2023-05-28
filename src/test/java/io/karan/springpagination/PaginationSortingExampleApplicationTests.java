e9e4f0a5d50d6d31a53167ca4d08099a6ebbc1cd
karka <k.kanojiya@tricentis.com>
2023-05-28 18:26:08 +0530

    Initial Commit

---
+++ src/test/java/io/karan/springpagination/PaginationSortingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.karan.springpagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationSortingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
